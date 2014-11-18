8e88c4b77f2c3934dc80acdf2f171f99f7bcbd08
tyler liu <replicat0r.misc@gmail.com>
2014-11-17 21:30:06 -0500

    add guard, initiated unit tests on static controller

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home Page"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "help Page"
+
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+    assert_select "title", "About Page"
+
+  end
+
+
+end
